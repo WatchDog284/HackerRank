cba2fa23a94c4a0383eefe09a0a760e2f1c416bc
WatchDog284 <ashishpro284@gmail.com>
2025-04-14 15:55:11 +0000

    renamed

--- 30 Days of Code/Day 12 - Inheritance/Solved.py
+++ 30 Days of Code/Day 12 - Inheritance/Solved.py
@@ -1,26 +1,23 @@
-# Define a base class `Person` to represent a generic person
 class Person:
-    # Constructor to initialize the first name, last name, and ID number of the person
+    # Constructor for the Person class to initialize first name, last name, and ID number
     def __init__(self, firstName, lastName, idNumber):
         self.firstName = firstName
         self.lastName = lastName
         self.idNumber = idNumber
 
-    # Method to print the person's details in a formatted way
+    # Method to print the person's details
     def printPerson(self):
         print("Name:", self.lastName + ",", self.firstName)
         print("ID:", self.idNumber)
 
-
-# Define a derived class `Student` that inherits from `Person`
 class Student(Person):
-    # Constructor to initialize the student details, including scores
+    # Constructor for the Student class, inheriting from Person
+    # Initializes first name, last name, ID number, and test scores
     def __init__(self, firstName, lastName, idNumber, scores):
-        # Call the parent class constructor to initialize common attributes
-        super().__init__(firstName, lastName, idNumber)
-        self.scores = scores
+        super().__init__(firstName, lastName, idNumber)  # Call the parent class constructor
+        self.scores = scores  # Store the test scores
 
-    # Method to calculate the grade based on the average score
+    # Method to calculate the grade based on the average of test scores
     def calculate(self):
         avg = sum(self.scores) / len(self.scores)  # Calculate the average score
         # Determine the grade based on the average score
@@ -37,24 +34,21 @@ class Student(Person):
         else:
             return 'T'  # Troll
 
-
 # Read input for the student's first name, last name, and ID number
 line = input().split()
 firstName = line[0]
 lastName = line[1]
 idNum = line[2]
 
-# Read the number of scores (not used in Python but part of the input format)
-numScores = int(input())
+# Read the number of scores (not used in Python) and the scores themselves
+numScores = int(input())  # Number of scores (not needed for processing in Python)
+scores = list(map(int, input().split()))  # Convert input scores to a list of integers
 
-# Read the scores as a list of integers
-scores = list(map(int, input().split()))
-
-# Create a `Student` object with the provided details
+# Create a Student object with the provided details
 s = Student(firstName, lastName, idNum, scores)
 
 # Print the student's personal details
 s.printPerson()
 
-# Print the student's grade based on the calculated average score
+# Print the student's grade based on the calculated average
 print("Grade:", s.calculate())
