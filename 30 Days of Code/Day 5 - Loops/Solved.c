fe88709a0c3aa8d0ec369f07da96591d473f3682
WatchDog284 <ashishpro284@gmail.com>
2025-04-14 11:20:22 +0000

    Day 5 added

---
+++ 30 Days of Code/Day 5 - Loops/Solved.c
@@ -0,0 +1,145 @@
+/*
+Steps of the program:
+1. Read an integer input from the user using the `readline` function.
+2. Trim any leading or trailing whitespace from the input using `ltrim` and `rtrim`.
+3. Convert the trimmed string input into an integer using the `parse_int` function.
+4. Use a `for` loop to iterate from 1 to 10.
+5. In each iteration, calculate the product of the input integer and the loop variable.
+6. Print the multiplication table in the format "n x i = result".
+7. Return 0 to indicate successful program execution.
+*/
+
+
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+
+int parse_int(char*);
+
+
+
+int main()
+{
+    int n = parse_int(ltrim(rtrim(readline())));
+    
+    // Print the multiplication table for the input integer n
+    // The loop iterates from 1 to 10
+    // In each iteration, it calculates the product of n and i
+    // and prints it in the format "n x i = result"
+    // The multiplication table is printed in the format "n x i = result"
+    for(int i = 1; i <= 10; i++){
+        printf("%d x %d = %d\n", n, i, n*i);
+    }
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
